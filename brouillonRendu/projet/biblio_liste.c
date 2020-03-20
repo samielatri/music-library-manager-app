4715a51172ec2140a3ea11c21cf0ebf8610a7527
samielatri <sami.elatri@yahoo.com>
2020-03-20 05:24:57 +0100

    q1.6 evol

--- brouillonRendu/projet/biblio_liste.c
+++ brouillonRendu/projet/biblio_liste.c
@@ -1,5 +1,10 @@
 /*	biblio_liste.c	*/
 
+/* 
+	Biblioth`eque permettant la construction d'une biblio (structure : liste)
+	`a partir de la lecture d'un fichier et la manipulation de cette biblio (liste)
+*/
+
 #include "biblio_liste.h"
 
 /* Alloue et retourne l'adresse d'une nouvelle Biblio */
@@ -51,49 +56,125 @@ void insere(Biblio *B, int num, char *titre, char *artiste) {
 	return ; 
 }
 
-void libere_biblio(Biblio *B)
-{
-    
+/* libere la biblio */
+void libere_biblio(Biblio *B) {
+    CellMorceau *temp = B->L ;
+	
+	while(temp != NULL) {
+		CellMorceau *temp1 = temp->suiv ; 
+
+		free(temp->artiste) ; 
+
+		if(temp->artiste != NULL) {
+			printf("Echec de la liberation memoire (biblio_liste/libere1)\n") ;
+			fprintf(stderr, "Echec de la liberation memoire biblio_liste/libere1\n") ; 
+			exit(EXIT_FAILURE) ; 
+		}
+
+		free(temp->titre) ;
+
+		if(temp->titre != NULL) {
+			printf("Echec de la liberation memoire (biblio_liste/libere2)\n") ;
+			fprintf(stderr, "Echec de la liberation memoire biblio_liste/libere2\n") ; 
+			exit(EXIT_FAILURE) ; 
+		}
+
+		free(temp) ; 
+
+		if(temp != NULL) {
+			printf("Echec de la liberation memoire (biblio_liste/libere3)\n") ;
+			fprintf(stderr, "Echec de la liberation memoire biblio_liste/libere3\n") ; 
+			exit(EXIT_FAILURE) ; 
+		}
+
+		temp = temp1 ; 
+	}
+
+	free(B) ;
+
+	if(temp != NULL) {
+		printf("Echec de la liberation memoire (biblio_liste/libereF)\n") ;
+		fprintf(stderr, "Echec de la liberation memoire biblio_liste/libereF\n") ; 
+		exit(EXIT_FAILURE) ; 
+	}
+
+	return ;
 }
 
-void afficheMorceau(CellMorceau *cell)
-{
+void afficheMorceau(CellMorceau *cell) {
 	printf("$L$ %8d $ %-32.32s $ %-32.32s $L$\n", cell->num, cell->titre, cell->artiste);
 }
 
-void affiche(Biblio *B)
-{
+void affiche(Biblio *B) {
    
 }
 
-Biblio *uniques (Biblio *B)
-{
+Biblio *uniques (Biblio *B) {
     
 }
 
-CellMorceau * rechercheParNum(Biblio *B, int num)
-{
+/* 
+	Affiche tous les morceaux contenus dans une biblio dont le numero d'enregistrement est num 
+	En bonus, elle affiche le nombre morceaux trouv'es ! 
+*/
+CellMorceau * rechercheParNum(Biblio *B, int num) {
+	CellMorceau *temp = B->L ;
+	int cpt = 0 ; // compteur du nombre de morceaux trouv'es
+	// on n'a jamais mentionné le fait que num (enregistrement) soit unique ou qu'on puisse pas avoir le m^eme plusieurs fois
+	while(temp != NULL) {
+		if( (temp->num) == num ) {
+			printf("%d\t%s\t%s\n", (temp->num), (temp->titre), (temp->artiste)) ;
+			++ cpt ; 
+		}
+		temp = (temp->suiv) ; 
+	}
+
+	/* Bonus : affichage du nombre de morceaux trouv'es */
+	if ( cpt == 0 ) {
+		printf("Aucun morceau n'a 'et'e trouv'e !\n") ;
+	} else {
+		printf("\n%d morceau(x) trouv'e(s).\n", cpt) ;
+	}
+
+	return ; 
+}
+
+/* 
+	Affiche tous les morceaux contenus dans une biblio dont le titre est titre 
+	En bonus, elle affiche le nombre morceaux trouv'es ! 
+*/
+CellMorceau *rechercheParTitre(Biblio *B, char * titre) {
+	CellMorceau *temp = B->L ;
+	int cpt = 0 ; // compteur du nombre de morceaux trouv'es
+	// on n'a jamais mentionné le fait que num (enregistrement) soit unique ou qu'on puisse pas avoir le m^eme plusieurs fois
+	while(temp != NULL) {
+		if( strcmp(temp->titre) == 0 ) {
+			printf("%d\t%s\t%s\n", (temp->num), (temp->titre), (temp->artiste)) ;
+			++ cpt ; 
+		}
+		temp = (temp->suiv) ; 
+	}
+
+	/* Bonus : affichage du nombre de morceaux trouv'es */
+	if ( cpt == 0 ) {
+		printf("Aucun morceau n'a 'et'e trouv'e !\n") ;
+	} else {
+		printf("\n%d morceau(x) trouv'e(s).\n", cpt) ;
+	}
+
+	return ; 		
+}
+
+Biblio *extraireMorceauxDe(Biblio *B, char * artiste) {
 	
 }
 
-CellMorceau *rechercheParTitre(Biblio *B, char * titre)
-{
+
+void insereSansNum(Biblio *B, char *titre, char *artiste) {
 	
 }
 
-Biblio *extraireMorceauxDe(Biblio *B, char * artiste)
-{
-	
-}
-
-
-void insereSansNum(Biblio *B, char *titre, char *artiste)
-{
-	
-}
-
-int supprimeMorceau(Biblio *B, int num)
-{
+int supprimeMorceau(Biblio *B, int num) {
 	
 }
 
