7cf5cfd573189f57dd86d15d083d0ee130359b76
samielatri <sami.elatri@yahoo.com>
2020-03-20 05:28:40 +0100

    q1.6 evol

--- add+commit+push.sh
+++ add+commit+push.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+echo "debut\n"
+
 git add -A
+echo "etape1->ok\n"
+
 git commit -m "${1}"
+echo "etape2->ok\n"
+
 git push
-echo "PUSH SUCCES"
+echo "etape3->ok\n"
+
+echo "FIN\n"
+
 exit 0
